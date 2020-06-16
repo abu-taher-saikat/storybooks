376df5b2de87ffa5730505d116ee90b2356831e9
saikat abu taher <mdabutahersaikat@gmail.com>
2020-06-16 10:14:42 +0600

    Initail commit

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+const express = require('express')
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+
+app.get('/',(req, res)=>{
+    res.send('welcome saikat');
+    
+})
+
+
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`)
+});
