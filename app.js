492f74c938e58c172f21d2dd3f1a52cb13ff0985
saikat abu taher <mdabutahersaikat@gmail.com>
2020-06-16 12:07:29 +0600

    done

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
-// passport config 
- require('./config/passport')(passport);
-
-
-// Load router ;
-const auth = require('./routes/auth');
-// const passport = require('./config/passport');
-
-
-
-
+app.get('/', (req, res) => {
+  res.send('It Works!');
+});
 
 // Use Routes
 app.use('/auth', auth);
 
-
-app.get('/',(req, res)=>{
-    res.send('welcome saikat');
-    
-})
-
-// connecting port
 const port = process.env.PORT || 5000;
-app.listen(port, ()=>{
-    console.log(`Server started on port ${port}`)
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
 });
--- routes/auth.js
+++ routes/auth.js
@@ -2,20 +2,11 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
+router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
-
-router.get('/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-
-router.get('/google/callback',
-    passport.authenticate('google', {
-        failureRedirect: '/'
-    }),
-     (req, res)=>{
-        // Successful authentication , redirect home
-        res.redirect('/dashboard');
-    })
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),(req, res) => {
+    res.redirect('/dashboard');
+  });
 
 module.exports = router;
