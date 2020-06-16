e662f7b5ab748b7c2b1d081be7f0fddf096c551f
saikat abu taher <mdabutahersaikat@gmail.com>
2020-06-16 11:38:37 +0600

    handel auth

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 
 const app = express();
+
+// passport config 
+ require('./config/passport')(passport);
+
+
+// Load router ;
+const auth = require('./routes/auth');
+// const passport = require('./config/passport');
+
+
+
+
+
+// Use Routes
+app.use('/auth', auth);
 
 
 app.get('/',(req, res)=>{
@@ -10,10 +26,7 @@ app.get('/',(req, res)=>{
     
 })
 
-
-
-
-
+// connecting port
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`)
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports={
+    mongoURI : 'mongodb+srv://saikat:saikat1095@cluster0-htwdq.mongodb.net/storybooks-dev?retryWrites=true&w=majority',
+    googleClientID : '515719603056-66ie9ivd8q62rddpvidrpaibekihj6sn.apps.googleusercontent.com',
+    googleClientSecret : '_JkN1-1ApCYYRbBrQKjMN1AT'
+}
